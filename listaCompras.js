43403b965e4e4546cf2f98dc8da33b141d3ecf1e
marcelohferreira <mhferreira1991@gmail.com>
2020-12-01 19:22:08 -0300

    Adicionado pedidos de Compra
    Pedidos de compra e consulta de pedidos de compra

---
+++ listaCompras.js
@@ -0,0 +1,9 @@
+let linhasTabela =[
+[4855,"Industria de Aluminios Tramonti LTDA","20/11/2020","4.400,00",],
+[137,"Industria e Comercio de Plasticos Plastil LTDA","13/11/2020","1.288,50",],
+[2471,"Madeireira SoMadeira e Materiais LTDA ME","07/11/2020","9.883,00",],
+[5574,"Distribuidora de Utensilios Domestilar","29/10/2020","2.210,00",],
+[4855,"Plasutil Industria e Comercio de Plasticos","25/10/2020","3.000,00",],
+[23174,"Mais Utilidades Distribuidora de Produtos LTDA","12/10/2020","3.899,00",],
+[47114,"Pindamonhangaba Industria de Vidros","01/10/2020","6.900,00",],
+]
---
+++ tabelaCompras.js
@@ -0,0 +1,41 @@
+function criaTag(elemento){
+	return document.createElement(elemento);
+}
+let tabela = document.getElementById("tabela");
+let thead = criaTag("thead");
+let tbody = criaTag("tbody");
+
+let indice = ["Numero Nota Entrada", "Fornecedor", "Data Compra","Valor Pedido"];
+let linhaHead = criaTag("tr");
+
+function criaCelula(tag,text){
+	tag = criaTag(tag);
+	tag.textContent = text;
+	return tag;
+}
+for(let j = 0; j<indice.length; j++){
+	let th = criaCelula("th",indice[j]);
+	linhaHead.appendChild(th);
+}
+thead.appendChild(linhaHead);
+
+for(let j=0, linhaBody = ''; j<linhasTabela.length; j++){
+	linhaBody = criaTag("tr");
+	for(let i=0, cel =''; i<linhasTabela[j].length; i++){
+		cel = criaCelula("td",linhasTabela[j][i]);
+		linhaBody.appendChild(cel)
+	}
+	tbody.appendChild(linhaBody);
+}
+
+tabela.appendChild(thead);
+tabela.appendChild(tbody);
+
+function filtrar() {
+    var termo = $('#busca').val().toUpperCase();
+            $('#tabela').each(function() { 
+               if($(this).html().toUpperCase().indexOf(termo) === -1) {
+                   $(this).hide();
+               }
+            });
+        }
