607144fca8fa5b15f304f16562c3b0ce47949996
marcelohferreira <mhferreira1991@gmail.com>
2020-12-01 19:27:54 -0300

    Adicionando tela de controle de estoque
    Telas de controle de estoque, manutencao e busca

---
+++ listadeEstoque.js
@@ -0,0 +1,19 @@
+let linhasTabela =[
+["T-4517as","Taca Americana 1 unidade","Tramonti","48,55",155],
+["C-45784","Copo Termico 500ml","Istanli","163,90",57],
+["12355","Toalha de mesa Rafanel","Rafanel","28,66",22],
+["R-787844","Jogo Panelas Travoxi c/5 unidades","Travoxi","423,44",12],
+["F-12147","Frigideira Antiaderente UltraMax","Ultra","37,10",6],
+["486855","Garrafa Termica 5L com tampa aco","Garrafos","66.70",0],
+["789845","Suporte para Televisor 43'' SuportMais","SuportMais","11,00",32],
+["A-654565","Abridor de Garrafas com Ima","CauBoy","254,99",18],
+["AL-127845","Panela de Pressao 4.5L Panel","Panel","88,90",67],
+["TS-78741","Jogo Mesa de Jantar 4 cadeiras","Mesany","550,00",5],
+["RT-124792","Jogo de Talheres com 36 pecas","Tramonti","97,00",49],
+["98321457","Jogo de Copos UltraLex","UltraLex","16,99",12],
+["32475617","Kit Ferramentas com 250 peças","Tramonti","337,99",3],
+["4897561","Tabua para Churrasco 28,5cm x 16,7cm","Mesany","23,49",487],
+["33117897","Processador de frutas 110v","Arnou","248,55",29],
+["4897561","Liquidificador Arnou 110v 3.5L","Arnou","83,99",87],
+["33117897","Fogao 5 bocas Parizi","Parizi","799,90",11]
+]
