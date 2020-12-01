bf0da7b6e9d2909529cfe649b6294d574bf72b3e
Joao Zanne <joaolzanne@gmail.com>
2020-11-30 22:27:41 -0300

    Tela de produtos, e cadastro
    Informações do produto

---
+++ cadastroItem.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+
+   $nomeProduto = $_POST['nome_produto'];   
+   $codigoItem = $_POST['codigo_item'];
+   $valorVenda = $_POST['Valor_venda'];
+   $marca = $_POST['marca'];
+   $descricao = $_POST['descricao'];
+   $pesoItem = $_POST['pesoItem'];
+   $altura = $_POST['altura'];
+   $largura = $_POST['largura'];
+   $profundidade = $_POST['profundidade'];
+?>
